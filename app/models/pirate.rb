e5d431fb19e8f2b4402b07ba6c0dbe2277f389f0
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-03-03 17:43:43 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+  attr_reader :name, :weight, :height
+  
+  @@all = []
+    
+    def initialize(params)
+      @name = params[:name]
+      @weight = params[:weight]
+      @height = params[:height]
+    end 
+    
+    def self.all
+      @@all 
+    end 
+    
+    
 end
+
+
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,38 @@
 class Ship
+  attr_reader :name, :type, :booty 
+  
+  @@all = []
+  
+    def initalize(params)
+      @name = params[:name]
+      @weight = params[:weight]
+      @height = params[:height]
+    end 
+    
+    def self.all 
+      @@all 
+    end 
+    
+    def self.clear 
+      @@all = [] 
+    end 
+  
 end
+
+# class Pirate
+#   attr_reader :name, :weight, :height
+  
+#   @@all = []
+    
+#     def initialize(params)
+#       @name = params[:name]
+#       @weight = params[:weight]
+#       @height = params[:height]
+#     end 
+    
+#     def self.all
+#       @@all 
+#     end 
+    
+    
+# end

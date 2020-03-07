4509933acb560cbc62178da20bd35b9dd477972e
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-03-07 17:42:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,6 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
     get '/' do
       erb :root
     end
@@ -11,18 +10,17 @@ module FormsLab
     get '/new' do
       erb :'pirates/new'
     end
-    
+
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
- 
+
       params[:pirate][:ships].each do |details|
         Ship.new(details)
       end
- 
       @ships = Ship.all
- 
-        erb :'pirates/new'
-      end
 
-     end
+      erb :'pirates/show'
+    end
+
+  end
 end
